8c431e4cf5e87dfe14ec316e3f3bb5f5a597804f
jlewitt1 <josh.lewittes@gmail.com>
2023-06-18 23:02:02 +0300

    update mlflow example based on latest release with Runs

--- mlflow_vs_runhouse/runhouse/re_run_training.py
+++ mlflow_vs_runhouse/runhouse/re_run_training.py
@@ -10,50 +10,52 @@ def re_run_training(alpha, l1_ratio):
     """Once resources have been saved via Runhouse, we can easily re-run the whole training pipeline
     (e.g. with new alpha or l1 ratio or an updated dataset)"""
 
-    with rh.Run(name=f"training_run_{int(time.time())}"):
-        # Note: Set dryrun to ``True`` when loading these resources as we do not need to re-instantiate anything
-        # If we were re-running these services on a different cluster, then we should not set dryrun to True
-        table = rh.table("raw_data", dryrun=True)
+    # Note: Set dryrun to ``True`` when loading these resources as we do not need to re-instantiate anything
+    # If we were re-running these services on a different cluster, then we should not set dryrun to True
+    table = rh.table("raw_data")
 
-        split_data = rh.function(name="split_data", dryrun=True)
-        train, test, train_x, train_y, test_x, test_y = split_data(table)
+    split_data = rh.function(name="split_data")
+    train, test, train_x, train_y, test_x, test_y = split_data(table)
 
-        # Run the model fitting on the cpu cluster
-        fit_model = rh.function("fit_model", dryrun=True)
-        lr = fit_model(alpha, l1_ratio, train_x, train_y)
+    # Run the model fitting on the cpu cluster
+    fit_model = rh.function("fit_model")
+    lr = fit_model(alpha, l1_ratio, train_x, train_y)
 
-        make_predictions = rh.function(name="make_predictions", dryrun=True)
-        predicted_qualities = make_predictions(lr, test_x)
+    make_predictions = rh.function(name="make_predictions")
+    predicted_qualities = make_predictions(lr, test_x)
 
-        eval_metrics = rh.function(name="eval_metrics", dryrun=True)
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    eval_metrics = rh.function(name="eval_metrics")
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-        print("RMSE: %s" % rmse)
-        print("MAE: %s" % mae)
-        print("R2: %s" % r2)
+    print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+    print("RMSE: %s" % rmse)
+    print("MAE: %s" % mae)
+    print("R2: %s" % r2)
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
-        else:
-            mlflow.sklearn.log_model(lr, "model")
+    # Model registry does not work with file store
+    if tracking_url_type_store != "file":
+        # Register the model
+        # There are other ways to use the Model Registry, which depends on the use case,
+        # please refer to the doc for more information:
+        # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+        mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+    else:
+        mlflow.sklearn.log_model(lr, "model")
 
 
 if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    re_run_training(alpha, l1_ratio)
+    with rh.Run(name=f"training_run_{int(time.time())}") as r:
+        re_run_training(alpha, l1_ratio)
+
+    r.save()
--- mlflow_vs_runhouse/runhouse/train.py
+++ mlflow_vs_runhouse/runhouse/train.py
@@ -77,54 +77,50 @@ def run_training(alpha, l1_ratio):
     # Initialize a CPU cluster to hold each of the functions / microservices needed for training our model
     cpu = rh.cluster("^rh-cpu").up_if_not()
 
-    with rh.Run(name=f"train_run_{int(time.time())}") as r:
-        # Create a microservice for splitting the data on the cpu cluster
-        split_data = rh.function(split_data_into_training_and_test, name="split_data").to(cpu, reqs=['scikit-learn',
-                                                                                                     'pandas<2.0.0',
-                                                                                                     's3fs']).save()
-        # Run the data splitting on the cluster
-        train, test, train_x, train_y, test_x, test_y = split_data(table)
+    # Create a microservice for splitting the data on the cpu cluster
+    split_data = rh.function(split_data_into_training_and_test, name="split_data").to(cpu, reqs=['scikit-learn',
+                                                                                                 'pandas<2.0.0',
+                                                                                                 's3fs']).save()
+    # Run the data splitting on the cluster
+    train, test, train_x, train_y, test_x, test_y = split_data(table)
 
-        # Create a microservice for fitting the model on the cpu cluster
-        fit_model = rh.function(fn=fit_model_on_training_data, name="fit_model").to(cpu, reqs=['scikit-learn', 'mlflow',
-                                                                                               'pandas<2.0.0']).save()
+    # Create a microservice for fitting the model on the cpu cluster
+    fit_model = rh.function(fn=fit_model_on_training_data, name="fit_model").to(cpu, reqs=['scikit-learn', 'mlflow',
+                                                                                           'pandas<2.0.0']).save()
 
-        # Run the model fitting on the cpu cluster
-        lr = fit_model(alpha, l1_ratio, train_x, train_y)
+    # Run the model fitting on the cpu cluster
+    lr = fit_model(alpha, l1_ratio, train_x, train_y)
 
-        # Create a microservice for making model predictions on the cpu cluster
-        make_predictions = rh.function(make_test_predictions, name="make_predictions").to(cpu).save()
-        predicted_qualities = make_predictions(lr, test_x)
+    # Create a microservice for making model predictions on the cpu cluster
+    make_predictions = rh.function(make_test_predictions, name="make_predictions").to(cpu).save()
+    predicted_qualities = make_predictions(lr, test_x)
 
-        # Create a microservice evaluating the model metrics, and have it live on the cpu cluster
-        eval_metrics = rh.function(eval_model, name="eval_metrics").to(cpu, reqs=['scikit-learn']).save()
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    # Create a microservice evaluating the model metrics, and have it live on the cpu cluster
+    eval_metrics = rh.function(eval_model, name="eval_metrics").to(cpu, reqs=['scikit-learn']).save()
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-        print("RMSE: %s" % rmse)
-        print("MAE: %s" % mae)
-        print("R2: %s" % r2)
+    print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+    print("RMSE: %s" % rmse)
+    print("MAE: %s" % mae)
+    print("R2: %s" % r2)
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
-        else:
-            mlflow.sklearn.log_model(lr, "model")
-
-    # save this Run to Runhouse Den
-    r.save()
+    # Model registry does not work with file store
+    if tracking_url_type_store != "file":
+        # Register the model
+        # There are other ways to use the Model Registry, which depends on the use case,
+        # please refer to the doc for more information:
+        # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+        mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+    else:
+        mlflow.sklearn.log_model(lr, "model")
 
 
 if __name__ == "__main__":
@@ -134,4 +130,8 @@ if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    run_training(alpha, l1_ratio)
+    with rh.Run(name=f"train_run_{int(time.time())}") as r:
+        run_training(alpha, l1_ratio)
+
+    # save this Run locally to the `rh` folder of this working directory
+    r.save()
